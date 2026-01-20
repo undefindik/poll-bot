22e844ead7fad0b85b610765fdeafb826d8357e4
Dmitry <f.dimas.pl@gmail.com>
2026-01-21 01:35:20 +0300

    Исправлены ошибки деактивации

--- src/handlers/command/deactivate.js
+++ src/handlers/command/deactivate.js
@@ -1,16 +1,30 @@
 import USER_MESSAGES from "../../messages/user.messages.js";
 import {InlineKeyboard} from "grammy";
+import {supabase as database} from "../../database.js";
 
 const deactivateCommand = async (ctx) => {
-    await ctx.reply(
-        USER_MESSAGES.warningDeactivate(), {
-            parse_mode: "HTML",
-            reply_markup: new InlineKeyboard()
-                .text("Да", "deactivate_verification_yes")
-                .row()
-                .text("Нет", "deactivate_verification_no")
-        }
-    )
+    const {data} = await database
+        .from('users')
+        .select("*")
+        .eq('telegram_id', ctx.from.id)
+        .single()
+
+
+    console.log(data)
+
+    if (data?.is_verification) {
+        await ctx.reply(
+            USER_MESSAGES.warningDeactivate(), {
+                parse_mode: "HTML",
+                reply_markup: new InlineKeyboard()
+                    .text("Да", "deactivate_verification_yes")
+                    .row()
+                    .text("Нет", "deactivate_verification_no")
+            }
+        )
+    } else {
+        await ctx.reply("Вы не верифицированы")
+    }
 }
 
 export default deactivateCommand;
--- src/handlers/query/deactivateVerification.js
+++ src/handlers/query/deactivateVerification.js
@@ -2,6 +2,8 @@ import {supabase as database} from "../../database.js"
 import USER_MESSAGES from "../../messages/user.messages.js";
 
 export const deactivateVerificationYes = async (ctx) => {
+    await ctx.deleteMessage();
+
     const {data, error} = await database
         .from('users')
         .delete()
